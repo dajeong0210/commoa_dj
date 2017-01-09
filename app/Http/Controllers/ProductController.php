41db364b9126ea4e0e870f18d1b64a1c32a34152
dajeong <lillian2.3lee@gmail.com>
2017-01-09 14:19:54 +0900

    commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::orderBy('created_at', 'desc')->paginate(10);
+        $products = Product::orderBy('id', 'desc')->paginate(10);
         return view('Product.index')->with('products', $products);
     }
 
