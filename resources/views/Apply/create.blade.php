57c76ec4cd6d18c4091f89b0d49796bfad60b1e0
HyeRi <yunhyel2@naver.com>
2017-01-09 18:06:21 +0900

    has-error

--- resources/views/Apply/create.blade.php
+++ resources/views/Apply/create.blade.php
@@ -3,74 +3,74 @@
 @section('content')
     <div class="page shop-apply">
         <h1>입점 신청 페이지</h1>
-        <form class="ShopApply" method="POST" action="{{ url('/apply') }}">
+        <form class="ShopApply" method="POST">
         {{ csrf_field() }}
             <div class="user-group group">
                 <h2>계정 정보 입력</h2>
                 <div class="form-group">
                     <label for="user_email">이메일(아이디)</label>
-                    <input type="email" name="user_email" class="input user_email" value="{{ Auth::user()->email }}" readonly/>
+                    <input type="email" name="user_email" class="input user_email {{ $errors->has('user_email') ? 'has-error' : '' }}" value="{{ Auth::user()->email }}" readonly/>
                 </div>
             </div>
             <div class="shop-group group">
                 <h2>Shop 정보 입력</h2>
                 <div class="form-group half-layout">
                     <label for="shop_name">Shop 이름</label>
-                    <input type="text" name="shop_name" class="input shop_name required"/>
+                    <input type="text" name="shop_name" class="input shop_name required" value="{{ old('shop_name') }}"/>
                 </div>
                 <div class="form-group half-layout">
                     <label for="shop_url">Shop URL</label>
-                    <input type="text" name="shop_url" class="input shop_url required"/>
+                    <input type="text" name="shop_url" class="input shop_url required" value="{{ old('shop_url') }}"/>
                 </div>
             </div>
             <div class="business-group group">
                 <h2>사업자 정보 입력</h2>
                 <div class="form-group half-layout">
                     <label for="business_name">상호(법인명)</label>
-                    <input type="text" name="business_name" class="input business_name required"/>
+                    <input type="text" name="business_name" class="input business_name required" value="{{ old('business_name') }}"/>
                 </div>
                 <div class="form-group half-layout">
                     <label for="business_ceo">대표자명</label>
-                    <input type="text" name="business_ceo" class="input business_ceo required"/>
+                    <input type="text" name="business_ceo" class="input business_ceo required" value="{{ old('business_ceo') }}"/>
                 </div>
                 <div class="form-group">
                     <label for="business_address">사업장주소</label>
-                    <input type="address" name="business_address" class="input business_address required"/>
+                    <input type="address" name="business_address" class="input business_address required" value="{{ old('business_address') }}"/>
                 </div>
                 <div class="form-group half-layout">
                     <label for="business_no">사업자등록번호</label>
-                    <input type="text" name="business_no" class="input business_no required digits" placeholder="'-'를 제외한 숫자만 입력해주세요."/>
+                    <input type="text" name="business_no" class="input business_no required digits" placeholder="'-'를 제외한 숫자만 입력해주세요." value="{{ old('business_no') }}"/>
                 </div>
                 <div class="form-group half-layout">
                     <label for="business_sale_no">통신판매번호</label>
-                    <input type="text" name="business_sale_no" class="input business_sale_no required"/>
+                    <input type="text" name="business_sale_no" class="input business_sale_no required" value="{{ old('business_sale_no') }}"/>
                 </div>
                 <div class="form-group half-layout">
                     <label for="business_docu">사업자등록증</label>
-                    <input type="file" name="business_docu" class="input business_docu required"/>
+                    <input type="file" name="business_docu" class="input business_docu required" value="{{ old('business_docu') }}"/>
                 </div>
                 <div class="form-group half-layout">
                     <label for="sale_docu">통신판매업증</label>
-                    <input type="file" name="sale_docu" class="input sale_docu required"/>
+                    <input type="file" name="sale_docu" class="input sale_docu required" value="{{ old('sale_docu') }}"/>
                 </div>
             </div>
             <div class="contact-group group">
                 <h2>쇼핑몰 담당자 정보 입력</h2>
                 <div class="form-group half-layout">
                     <label for="contact_name">이름</label>
-                    <input type="text" name="contact_name" class="input contact_name required"/>
+                    <input type="text" name="contact_name" class="input contact_name required" value="{{ old('contact_name') }}"/>
                 </div>
                 <div class="form-group half-layout">
                     <label for="contact_email">이메일</label>
-                    <input type="email" name="contact_email" class="input contact_email required" data-rule-required="true" data-rule-email="true" data-msg-email="이메일 형식이 옳지 않습니다!"/>
+                    <input type="email" name="contact_email" class="input contact_email required" data-rule-required="true" data-rule-email="true" data-msg-email="이메일 형식이 옳지 않습니다!" value="{{ old('contact_email') }}"/>
                 </div>
                 <div class="form-group half-layout">
                     <label for="contact_mobile">핸드폰</label>
-                    <input type="phone" name="contact_mobile" class="input contact_mobile required digits" placeholder="'-'를 제외한 숫자만 입력해주세요."/>
+                    <input type="phone" name="contact_mobile" class="input contact_mobile required digits" placeholder="'-'를 제외한 숫자만 입력해주세요." value="{{ old('contact_mobile') }}"/>
                 </div>
                 <div class="form-group half-layout">
                     <label for="contact_phone">전화</label>
-                    <input type="phone" name="contact_phone" class="input contact_phone" placeholder="'-'를 제외한 숫자만 입력해주세요."/>
+                    <input type="phone" name="contact_phone" class="input contact_phone" placeholder="'-'를 제외한 숫자만 입력해주세요." value="{{ old('contact_phone') }}"/>
                 </div>
             </div>
             <div class="notice-group group">
