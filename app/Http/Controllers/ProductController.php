5ab90785774e52204e826fcc29f3423c6f493fa0
dajeong <lillian2.3lee@gmail.com>
2017-01-13 11:59:49 +0900

    add welcome-Controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Product;
 use Illuminate\Support\Facades\DB;
+use App\Cpu;
 
 class ProductController extends Controller
 {
