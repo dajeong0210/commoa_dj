f5a52a59340753388509fe017d86104c0ff0d722
HyeRi <yunhyel2@naver.com>
2017-01-16 10:42:14 +0900

    main

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Product;
 use App\Shop;
+use App\Category;
 use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
@@ -23,8 +24,8 @@ class WelcomeController extends Controller
         $products3 = Category::where('id', 3)->first()->products()->orderBy('views', 'desc')->limit(4)->get(); 
         $shops = Shop::orderBy('id', 'asc')->selectRaw('*, @row:=@row+1 as row')->limit(8)->get();
 
-        return view('welcome')->with('product1', $product1)->with('product2', $product2)
-                ->with('product3', $product3)->with('shops', $shops);
+        return view('welcome')->with('products1', $products1)->with('products2', $products2)
+                ->with('products3', $products3)->with('shops', $shops);
     }
 
     /**
