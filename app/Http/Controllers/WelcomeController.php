da62dad0bcd25dd8a8fe208d869626665b15453a
dajeong <lillian2.3lee@gmail.com>
2017-01-16 10:39:32 +0900

    welcome controller

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -18,10 +18,13 @@ class WelcomeController extends Controller
     {
         DB::statement(DB::raw('set @row:=0'));
 
-        $products = Product::orderBy('views','desc')->selectRaw('*, @row:=@row+1 as row')->limit(8)->get();
+        $products1 = Category::where('id', 1)->first()->products()->orderBy('views', 'desc')->limit(4)->get(); 
+        $products2 = Category::where('id', 2)->first()->products()->orderBy('views', 'desc')->limit(4)->get(); 
+        $products3 = Category::where('id', 3)->first()->products()->orderBy('views', 'desc')->limit(4)->get(); 
         $shops = Shop::orderBy('id', 'asc')->selectRaw('*, @row:=@row+1 as row')->limit(8)->get();
 
-        return view('welcome')->with('products', $products)->with('shops', $shops);
+        return view('welcome')->with('product1', $product1)->with('product2', $product2)
+                ->with('product3', $product3)->with('shops', $shops);
     }
 
     /**
