a4258b6b2bf50bfa82a2003516e03fcbc17acf5f
dajeong <lillian2.3lee@gmail.com>
2017-01-18 17:19:56 +0900

    edit myPage

--- resources/views/Mypage/edit.blade.php
+++ resources/views/Mypage/edit.blade.php
@@ -12,7 +12,7 @@
                     <label for="user_email">이메일(아이디)</label>
                     <input type="email" name="user_email" class="input user_email" value="{{ Auth::user()->email }}" disabled/>
                 </div>
-                <div class="form-group">
+                <div class="form-group{{ $errors->has('user_name') ? ' has-error' : '' }}">
                     <label for="user_name">이름</label>
                     <input type="text" name="user_name" class="input user_name" value="{{ Auth::user()->name }}"/>
                 </div>
@@ -20,7 +20,7 @@
                     <label for="old_password">전 비밀번호</label>
                     <input type="password" name="old_password" class="input old_password" value="" />
                 </div>
-                <div class="form-group half-layout">
+                <div class="form-group half-layout{{ $errors->has('password') ? ' has-error' : '' }}">
                     <label for="new_password">바꿀 비밀번호</label>
                     <input type="password" name="password" class="input password" value=""/>
                 </div>
