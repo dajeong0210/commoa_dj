4764858b278430fa250136e4eb83edbcfddf69a4
dajeong <lillian2.3lee@gmail.com>
2017-01-18 11:57:22 +0900

    Mypage controller1

--- resources/views/layouts/myLayout.blade.php
+++ resources/views/layouts/myLayout.blade.php
@@ -1,7 +1,7 @@
 <nav class="tap-nav">
     <ul>
         <li class="{{ Request::segment(1) == 'mypage' ? 'active' : '' }}"><a href="{{ url('/mypage') }}">메인</a></li>
-        <li class="{{ Request::segment(3) == 'edit' ? 'active' : '' }}"><a href="{{ url('/user') . '/' . Auth::user()->id . '/edit' }}">개인정보수정</a></li>
+        <li class="{{ Request::segment(3) == 'edit' ? 'active' : '' }}"><a href="{{ url('/myinfo') }}">개인정보수정</a></li>
         <li class="{{ Request::segment(1) == 'bookmark' ? 'active' : '' }}"><a href="{{ url('/bookmark') }}">북마크</a></li>
         <li class="{{ Request::segment(1) == 'favorite' ? 'active' : '' }}"><a href="{{ url('/favorite') }}">찜한상품</a></li>
         @if( Auth::user()->permission == '1' )
