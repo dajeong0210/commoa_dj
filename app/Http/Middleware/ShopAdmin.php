1910d008408460015454ac1f9f70f8725000c42b
HyeRi <yunhyel2@naver.com>
2017-01-20 18:08:55 +0900

    none

--- app/Http/Middleware/ShopAdmin.php
+++ app/Http/Middleware/ShopAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class ShopAdmin
 {
@@ -16,7 +17,7 @@ class ShopAdmin
     public function handle($request, Closure $next)
     {
         if(Auth::user()->permission != 1){
-            return redirect('/');
+            return back();
         }
 
         return $next($request);
