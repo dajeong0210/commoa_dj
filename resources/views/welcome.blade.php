7cb11b70a473e921f9a4c5ea0a545f46bd7372d6
dajeong <lillian2.3lee@gmail.com>
2017-01-23 09:12:16 +0900

    main page href

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,13 +91,13 @@
             @foreach( $products1 as $product )
             <li>
                 <span class="badge recommend">추천!!</span>
-                <a href="{{ $product->url }}">
+                <a href="{{ url('/product') . '/' . $product->id }}">
                 <div class="img-box prod">
                     <img src="{{ $product->image }}" alt="">
                 </div>
                 </a>
                 <div class="detail-box">
-                    <a class="prod-title" href="{{ $product->url }}"> {{ $product->name }} </a>
+                    <a class="prod-title" href="{{ url('/product') . '/' . $product->id }}"> {{ $product->name }} </a>
                     <ul class="prod_category">
                     @foreach( $product->categories as $category )
                         <li>
@@ -134,13 +134,13 @@
             @foreach( $products2 as $product )
             <li>
                 <span class="badge recommend">추천!!</span>
-                <a href="{{ $product->url }}">
+                <a href="{{ url('/product') . '/' . $product->id }}">
                 <div class="img-box prod">
                     <img src="{{ $product->image }}" alt="">
                 </div>
                 </a>
                 <div class="detail-box">
-                    <a class="prod-title" href="{{ $product->url }}"> {{ $product->name }} </a>
+                    <a class="prod-title" href="{{ url('/product') . '/' . $product->id }}"> {{ $product->name }} </a>
                     <ul class="prod_category">
                     @foreach( $product->categories as $category )
                         <li>
@@ -177,13 +177,13 @@
             @foreach( $products3 as $product )
             <li>
                 <span class="badge recommend">추천!!</span>
-                <a href="{{ $product->url }}">
+                <a href="{{ url('/product') . '/' . $product->id }}">
                 <div class="img-box prod">
                     <img src="{{ $product->image }}" alt="">
                 </div>
                 </a>
                 <div class="detail-box">
-                    <a class="prod-title" href="{{ $product->url }}"> {{ $product->name }} </a>
+                    <a class="prod-title" href="{{ url('/product') . '/' . $product->id }}"> {{ $product->name }} </a>
                     <ul class="prod_category">
                     @foreach( $product->categories as $category )
                         <li>
