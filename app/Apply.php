7128a1a9aa3522202526bc140b98abeafbdb7d1c
dajeong <lillian2.3lee@gmail.com>
2017-01-25 15:31:23 +0900

    reduce source code

--- app/Apply.php
+++ app/Apply.php
@@ -6,6 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Apply extends Model
 {
+    protected $fillable = [
+        'business_docu',
+        'sale_docu',
+        'user_email',
+        'shop_name',
+        'shop_url',
+        'business_name',
+        'business_ceo',
+        'business_address',
+        'business_no',
+        'business_sale_no',
+        'contact_name',
+        'contact_email',
+        'contact_mobile',
+        'contact_phone',
+    ];
+
+    protected $guarded= [
+        'id',
+        'business_docu',
+        'sale_docu',
+    ];
+
     public function user() 
     {
         return $this->belongsTo('App\User');
--- app/Http/Controllers/ApplyController.php
+++ app/Http/Controllers/ApplyController.php
@@ -35,26 +35,32 @@ class ApplyController extends Controller
         if( Auth::user()->apply()->count() != 0) {
             return view('apply.create');
         } else { 
-            $apply = new Apply;
-            $path_b = $request->file('business_docu')->store('images');
-            $path_s = $request->file('sale_docu')->store('images');
-            $apply->user_email = $request->input('user_email');
-            $apply->shop_name = $request->input('shop_name');
-            $apply->shop_url = $request->input('shop_url');
-            $apply->business_name = $request->input('business_name');
-            $apply->business_ceo = $request->input('business_ceo');
-            $apply->business_address = $request->input('business_address');
-            $apply->business_no = $request->input('business_no');
-            $apply->business_sale_no = $request->input('business_sale_no');
-            $apply->business_docu = $path_b;
-            $apply->sale_docu = $path_s;
-            $apply->contact_name = $request->input('contact_name');
-            $apply->contact_email = $request->input('contact_email');
-            $apply->contact_mobile = $request->input('contact_mobile');
-            $apply->contact_phone = $request->input('contact_phone');
-            $apply->user_id = User::where('email', $request->input('user_email'))->first()->id;
-            $apply->save();
+            $request->merge(['business_docu' => $request->file('business_docu')->store('images')]);
+            $request->merge(['sale_docu' => $request->file('sale_docu')->store('images')]);
+            $request = $request->except(['agree_01', 'agree_02']);
+            Auth::user()->apply()->create($request);
             return redirect('/');
+// before             
+            // $apply = new Apply;
+            // $path_b = $request->file('business_docu')->store('images');
+            // $path_s = $request->file('sale_docu')->store('images');
+            // $apply->user_email = $request->input('user_email');
+            // $apply->shop_name = $request->input('shop_name');
+            // $apply->shop_url = $request->input('shop_url');
+            // $apply->business_name = $request->input('business_name');
+            // $apply->business_ceo = $request->input('business_ceo');
+            // $apply->business_address = $request->input('business_address');
+            // $apply->business_no = $request->input('business_no');
+            // $apply->business_sale_no = $request->input('business_sale_no');
+            // $apply->business_docu = $path_b;
+            // $apply->sale_docu = $path_s;
+            // $apply->contact_name = $request->input('contact_name');
+            // $apply->contact_email = $request->input('contact_email');
+            // $apply->contact_mobile = $request->input('contact_mobile');
+            // $apply->contact_phone = $request->input('contact_phone');
+            // $apply->user_id = User::where('email', $request->input('user_email'))->first()->id;
+            // $apply->save();
+//
         } 
     }
 
@@ -76,29 +82,41 @@ class ApplyController extends Controller
     public function update(ApplyUpdateRequest $request, $id)
     {
         $apply = Apply::find($id);
-        if( $request->file('business_docu') != null ) { 
-            $path_b = $request->file('business_docu')->store('images');
-            $apply->business_docu = $path_b;
-        } 
-        if( $request->file('sale_docu') != null ) { 
-            $path_s = $request->file('sale_docu')->store('images');
-            $apply->sale_docu = $path_s;
+
+        if( $request->file('business_docu') != null ) {
+            $request->merge(['business_docu' => $request->file('business_docu')->store('images')]);
         }
-        $apply->user_email = $request->input('user_email');
-        $apply->shop_name = $request->input('shop_name');
-        $apply->shop_url = $request->input('shop_url');
-        $apply->business_name = $request->input('business_name');
-        $apply->business_ceo = $request->input('business_ceo');
-        $apply->business_address = $request->input('business_address');
-        $apply->business_no = $request->input('business_no');
-        $apply->business_sale_no = $request->input('business_sale_no');
-        $apply->contact_name = $request->input('contact_name');
-        $apply->contact_email = $request->input('contact_email');
-        $apply->contact_mobile = $request->input('contact_mobile');
-        $apply->contact_phone = $request->input('contact_phone');
-        $apply->user_id = User::where('email', $request->input('user_email'))->first()->id;
-        $apply->save();
-        return view('apply.edit')->with('apply', $apply);
+        if( $request->file('sale_docu') != null ) { 
+            $request->merge(['sale_docu' => $request->file('sale_docu')->store('images')]);
+        }
+        $request = $request->except(['_method', '_token']);
+        $apply->update($request);
+       
+        return redirect('apply/'.$apply->id.'/edit')->with('apply', $apply);
+//before 
+        // if( $request->file('business_docu') != null ) { 
+        //     $path_b = $request->file('business_docu')->store('images');
+        //     $apply->business_docu = $path_b;
+        // } 
+        // if( $request->file('sale_docu') != null ) { 
+        //     $path_s = $request->file('sale_docu')->store('images');
+        //     $apply->sale_docu = $path_s;
+        // }
+        // $apply->user_email = $request->input('user_email');
+        // $apply->shop_name = $request->input('shop_name');
+        // $apply->shop_url = $request->input('shop_url');
+        // $apply->business_name = $request->input('business_name');
+        // $apply->business_ceo = $request->input('business_ceo');
+        // $apply->business_address = $request->input('business_address');
+        // $apply->business_no = $request->input('business_no');
+        // $apply->business_sale_no = $request->input('business_sale_no');
+        // $apply->contact_name = $request->input('contact_name');
+        // $apply->contact_email = $request->input('contact_email');
+        // $apply->contact_mobile = $request->input('contact_mobile');
+        // $apply->contact_phone = $request->input('contact_phone');
+        // $apply->user_id = User::where('email', $request->input('user_email'))->first()->id;
+        // $apply->save();
+//
     }
 
     public function destroy($id)
