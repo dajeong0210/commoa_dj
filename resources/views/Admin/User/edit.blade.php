10c9726e5bd34c0ace162c1614ae99835ae0331f
HyeRi <yunhyel2@naver.com>
2017-02-02 14:42:11 +0900

    editpage modify

--- resources/views/Admin/User/edit.blade.php
+++ resources/views/Admin/User/edit.blade.php
@@ -4,21 +4,21 @@
     <div class="page mypage admin">
         @include('layouts.adminMenu') 
         <div class="form user-edit">
-            <h2>{{ $user->name }} 정보</h2>
+            <h2>회원정보</h2>
             <form class="user_edit validate" method="POST" action="{{ url('/admin') . '/' . $user->id }}">
             {{ method_field('put') }}
             {{ csrf_field() }}
             <div class="user-group group">
                 <div class="form-group one-layout form-group{{ $errors->has('user_email') ? ' has-error' : '' }}">
                     <label for="user_email">이메일(아이디)</label>
-                    <input type="email" name="user_email" class="input user_email" value="{{ Auth::user()->email }}" readonly/>
+                    <input type="email" name="user_email" class="input user_email" value="{{ $user->email }}" readonly/>
                     @if( $errors->has('user_email') )
                         <label class="error">{{ $errors->first('user_email') }}</label>
                     @endif
                 </div>
                 <div class="one-layout form-group form-group{{ $errors->has('user_name') ? ' has-error' : '' }}">
                     <label for="user_name">이름</label>
-                    <input type="text" name="user_name" class="input user_name required" value="{{ Auth::user()->name }}" required/>
+                    <input type="text" name="user_name" class="input user_name required" value="{{ $user->name }}" required/>
                     @if( $errors->has('user_name') )
                         <label class="error">{{ $errors->first('user_name') }}</label>
                     @endif
@@ -39,6 +39,14 @@
                     </div>
                 </div>
             </form>
+            @if( $user->permission == 1 )
+                <form class="ShopApply permission" method="POST" action="{{ url('/admin/permission') . '/' . $user->id }}">
+                {{ csrf_field() }}
+                    <div class="group">
+                        <input type="submit" value="일반회원으로" class="permission"/>
+                    </div>
+                </form>
+            @endif
         </div>
     </div>
 @endsection
