17679421d584bba3cbd564b63dbb220c4f7f70c7
HyeRi <yunhyel2@naver.com>
2017-02-13 11:26:52 +0900

    merge

--- resources/views/Product/index.blade.php
+++ resources/views/Product/index.blade.php
@@ -14,10 +14,13 @@
                         <h3 class="hidden">용도별</h3>
                         @foreach( $categories as $key=>$category )
                             @if( $key <4 )
-                                <a href="#"><div class="img-box" style="background:url({{ $category->image }}) center; background-size:cover;"></div></a>
-                            @endif
+                                <a href="#" ><div class="img-box" style="background:url({{ $category->image }}) center; background-size:cover;"></div></a>
                                 <label for="{{ 'purpose_' . $category }}" class="{{ isset($_GET['purpose'])? in_array( $category , $_GET['purpose'])? 'active' : '' : '' }}">{{ $category }}</label>
                                 <input type="checkbox" id="{{ 'purpose_' . $category }}" name="purpose[]" class="input purpose hidden" value="{{ $category }}" @if( isset($_GET['purpose'])) @if( in_array( $category , $_GET['purpose']) ) checked="checked" @endif @endif />
+                            @else
+                                <label for="{{ 'purpose_' . $category }}" class="{{ isset($_GET['purpose'])? in_array( $category , $_GET['purpose'])? 'active' : '' : '' }}">{{ $category }}</label>
+                                <input type="checkbox" id="{{ 'purpose_' . $category }}" name="purpose[]" class="input purpose hidden" value="{{ $category }}" @if( isset($_GET['purpose'])) @if( in_array( $category , $_GET['purpose']) ) checked="checked" @endif @endif />
+                            @endif
                         @endforeach
                     </div>
                     <div class="filter-group sortBy hidden">
