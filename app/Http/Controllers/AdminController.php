2d2575d199961b8c11a359c3221a480a31ac2882
dajeong <lillian2.3lee@gmail.com>
2017-02-28 14:54:25 +0900

    category

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -73,62 +73,29 @@ class AdminController extends Controller
 			$category->recommend_memory = null; 
 		}
 		
-		$min_cpus_string ="";
-		$min_vgas_string =""; 
-		$recommend_cpus_string =""; 
-		$recommend_vgas_string =""; 
 		if( $min_cpus != null ) {
-			foreach ( $min_cpus as $min_cpu ) {
-				if( $min_cpus_string == "" ) {
-					$min_cpus_string = $min_cpu;
-				} else {
-					$min_cpus_string = $min_cpus_string . "\n" . $min_cpu;
-				}
-			}
-			$category->min_cpu = $min_cpus_string;
+			$category->min_cpu = $min_cpus;
 		} else {
 			$category->min_cpu = null;
 		}
 
 		if( $min_vgas != null ) {
-			foreach ( $min_vgas as $min_vga ) {
-				if( $min_vgas_string == "" ) {
-					$min_vgas_string = $min_vga;
-				} else {
-					$min_vgas_string = $min_vgas_string . "\n" . $min_vga;
-				}
-			}
-			$category->min_vga = $min_vgas_string;
+			$category->min_vga = $min_vgas;
 		} else {
 			$category->min_vga = null;
 		}
 
 		if( $recommend_cpus != null ) {
-			foreach ( $recommend_cpus as $recommend_cpu ) {
-				if( $recommend_cpus_string == "" ) {
-					$recommend_cpus_string = $recommend_cpu;
-				} else {
-					$recommend_cpus_string = $recommend_cpus_string . "\n" . $recommend_cpu;
-				}
-			}
-			$category->recommend_cpu = $recommend_cpus_string;
+			$category->recommend_cpu = $recommend_cpus;
 		} else {
 			$category->recommend_cpu = null;
 		}
 
 		if( $recommend_vgas != null ) {
-			foreach ( $recommend_vgas as $recommend_vga ) {
-				if( $recommend_vgas_string == "" ) {
-					$recommend_vgas_string = $recommend_vga;
-				} else {
-					$recommend_vgas_string = $recommend_vgas_string . "\n" . $recommend_vga;
-				}
-			}
-			$category->recommend_vga = $recommend_vgas_string;
+			$category->recommend_vga = $recommend_vgas;
 		} else {
 			$category->recommend_vga = null;
 		}
-	
 		
 		if( $sort != null ) {
 			$category->sort = 1;
@@ -150,7 +117,8 @@ class AdminController extends Controller
 		$category->name = $request->input('category_name');
 		$category->color = $request->input('category_color');
 		$sort = $request->input('category_sort');
-		$image = $request->file('category_image');$category->color = $request->input('category_color');
+		$image = $request->file('category_image');
+		$category->color = $request->input('category_color');
 		$min_cpus = $request->input('min_cpu'); 
 		$min_vgas = $request->input('min_vga'); 
 		$recommend_cpus = $request->input('recommend_cpu'); 
@@ -177,63 +145,29 @@ class AdminController extends Controller
 			$category->recommend_memory = null; 
 		}
 
-		$min_cpus_string ="";
-		$min_vgas_string =""; 
-		$recommend_cpus_string =""; 
-		$recommend_vgas_string =""; 
-
 		if ( $min_cpus != null ) { 
-			foreach ( $min_cpus as $min_cpu ) {
-				if( $min_cpus_string == "" ) {
-					$min_cpus_string = $min_cpu;
-				} else {
-					$min_cpus_string = $min_cpus_string . "\n" . $min_cpu;
-				}
-			}
-			$category->min_cpu = $min_cpus_string;
+			$category->min_cpu = $min_cpus;
 		} else { 
 			$category->min_cpu = null;
 		}
 
 		if ( $min_vgas != null ) { 
-			foreach ( $min_vgas as $min_vga ) {
-				if( $min_vgas_string == "" ) {
-					$min_vgas_string = $min_vga;
-				} else {
-					$min_vgas_string = $min_vgas_string . "\n" . $min_vga;
-				}
-			}
-			$category->min_vga = $min_vgas_string;
+			$category->min_vga = $min_vgas;
 		} else { 
 			$category->min_vga = null; 
 		}
 
 		if ( $recommend_cpus != null ) { 
-			foreach ( $recommend_cpus as $recommend_cpu ) {
-				if( $recommend_cpus_string == "" ) {
-					$recommend_cpus_string = $recommend_cpu;
-				} else {
-					$recommend_cpus_string = $recommend_cpus_string . "\n" . $recommend_cpu;
-				}
-			}
-			$category->recommend_cpu = $recommend_cpus_string;
+			$category->recommend_cpu = $recommend_cpus;
 		} else { 
 			$category->recommend_cpu = null;
 		}
 
-		if ( $recommend_vgas != null ) { 
-			foreach ( $recommend_vgas as $recommend_vga ) {
-				if( $recommend_vgas_string == "" ) {
-					$recommend_vgas_string = $recommend_vga;
-				} else {
-					$recommend_vgas_string = $recommend_vgas_string . "\n" . $recommend_vga;
-				}
-			} 
-			$category->recommend_vga = $recommend_vgas_string;
+		if ( $recommend_vgas != null ) {  
+			$category->recommend_vga = $recommend_vgas;
 		} else { 
 			$category->recommend_vga = null; 
 		}
-
 
 		if( $category->sort != '0') {
 			if( $sort != null ) {
