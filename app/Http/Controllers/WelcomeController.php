45592372906167c3af72179370cc44116d031d0a
dajeong <lillian2.3lee@gmail.com>
2017-03-03 09:28:31 +0900

    main

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -18,21 +18,16 @@ class WelcomeController extends Controller
         DB::statement(DB::raw('set @row:=0'));
 
         $recommends = Product::where('recommend', '<>', 0)->orderBy('recommend', 'asc')->limit(4)->get();
-        $new_items = Product::orderBy('created_at', 'desc')->limit(4)->get();
-        $products1 = Product::orderBy('views', 'desc')->limit(4)->get(); 
+        $new_items = Product::orderBy('created_at', 'desc')->limit(8)->get();
+        $hot_items = Product::orderBy('views', 'desc')->limit(8)->get(); 
         $products_cnt = Product::count();
         $shops_cnt = Shop::count();
         $banners = Banner::get();
+        $shops = Shop::get();
 
-        // if( !Auth::guest() ) {
-        //     $favorites = Auth::user()->products()->orderBy('pivot_product_user.created_at', 'desc')->limit(3)->get();
-        // } else {
-        //     $favorites = null;
-        // }
-
-        return view('welcome')->with('new_items', $new_items)->with('products1', $products1)
+        return view('welcome')->with('new_items', $new_items)->with('hot_items', $hot_items)
                 ->with('products_cnt', $products_cnt)->with('shops_cnt', $shops_cnt)
-                ->with('banners', $banners)->with('recommends', $recommends);
+                ->with('banners', $banners)->with('recommends', $recommends)->with('shops', $shops);
     }
 
     public function privacy() {
