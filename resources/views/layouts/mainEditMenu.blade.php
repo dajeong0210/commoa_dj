b062cdc9e532353badc056854027bfdb84a38c9a
HyeRi <yunhyel2@naver.com>
2017-03-08 17:38:50 +0900

    layout footer modify

--- resources/views/layouts/mainEditMenu.blade.php
+++ resources/views/layouts/mainEditMenu.blade.php
@@ -1,7 +1,7 @@
 <nav class="tab-nav">
     <ul>
-        <li class="{{ Request::segment(1) == 'admin' && Request::segment(2) == 'banner' ? 'active' : '' }}"><a href="{{ url('/admin/banner') }}">메인슬라이드</a></li>
-        <li class="{{ Request::segment(1) == 'admin' && Request::segment(2) == 'advertisement' ? 'active' : '' }}"><a href="{{ url('/admin/advertisement') }}">광고배너</a></li>
-        <li class="{{ Request::segment(1) == 'admin' && Request::segment(2) == 'recommend' ? 'active' : '' }}"><a href="{{ url('/admin/recommend') }}">추천상품</a></li>
+        <li class="mainEdit {{ Request::segment(1) == 'admin' && Request::segment(2) == 'banner' ? 'active' : '' }}"><a href="{{ url('/admin/banner') }}">메인슬라이드</a></li>
+        <li class="mainEdit {{ Request::segment(1) == 'admin' && Request::segment(2) == 'advertisement' ? 'active' : '' }}"><a href="{{ url('/admin/advertisement') }}">광고배너</a></li>
+        <li class="mainEdit {{ Request::segment(1) == 'admin' && Request::segment(2) == 'recommend' ? 'active' : '' }}"><a href="{{ url('/admin/recommend') }}">추천상품</a></li>
     </ul>
 </nav>
